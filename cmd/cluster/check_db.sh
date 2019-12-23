5a0dbabe556a8facb45ec97a4ae896dd5a520774
516108736 <516108736@qq.com>
2019-12-23 17:53:05 +0800

    1753

--- cmd/cluster/check_db.sh
+++ cmd/cluster/check_db.sh
@@ -4,6 +4,7 @@ step=60 #间隔的秒数，不能大于60
 touch /tmp/status.txt
 for (( i = 0; i < 60; i=(i+1) )); do
  rm data.tar.gz
+ kill -9 `ps -ef | grep cluster_config | awk '{print $2}'`
  curl https://qkcmainnet-go.s3.amazonaws.com/data/2019-12-22.21:04:06.tar.gz --output data.tar.gz
  rm -rf /go/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data
  mkdir -p /go/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data
