8199e4f09123c45f72ca9dc4c190dbcfad2f6313
516108736 <516108736@qq.com>
2019-12-23 17:10:50 +0800

    1710

--- cmd/cluster/check_db.sh
+++ cmd/cluster/check_db.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 step=60 #间隔的秒数，不能大于60
+touch /tmp/status.txt
 for (( i = 0; i < 60; i=(i+1) )); do
  rm data.tar.gz
  curl https://qkcmainnet-go.s3.amazonaws.com/data/2019-12-22.21:04:06.tar.gz --output data.tar.gz
@@ -12,6 +13,6 @@ for (( i = 0; i < 60; i=(i+1) )); do
  go build
  chmod +x ./run_cluster.sh && ./run_cluster.sh  ../../mainnet/singularity/cluster_config_template.json --check_db
  DATE=`date +%Y-%m-%d.%H:%M:%S`
- echo $DATE >> finish.txt
+ echo $DATE >> /tmp/status.txt
  sleep $step
 done
