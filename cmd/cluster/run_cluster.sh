b88735e9402f1695f4d2f7b3ad185c82cbfc9cec
516108736 <516108736@qq.com>
2019-12-24 12:08:31 +0800

    1208

--- cmd/cluster/run_cluster.sh
+++ cmd/cluster/run_cluster.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 configPath=$1
+
 slaveInfo=`grep -Po 'ID[" :]+\K[^"]+' $configPath | grep S`
 
 # start slave
@@ -11,14 +12,9 @@ do
 	 eval $cmd
 done
 
-sleep 2s
+sleep 5s
 
 # start master
-cmd="./cluster --cluster_config "${configPath}" "
-
-if [  -n "$2" ] ;then
-    cmd="./cluster --cluster_config "${configPath}" "$2"  "
-fi
-
+cmd="./cluster --cluster_config "${configPath}" >>master.log 2>&1 &"
 echo $cmd
 eval $cmd
--- mainnet/snapshot/backup.sh
+++ mainnet/snapshot/backup.sh
@@ -2,36 +2,70 @@
 # script to sync data into s3:
 
 set -ex
-if [ -z "$BACKUP_DIR" ]; then
-	echo "'BACKUP_DIR'  var must be set"
-	exit 1
-fi
-DATA_DIR=$GOPATH/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/
-DATE=`date +%Y-%m-%d.%H:%M:%S`
-OUTPUT_FILE=$BACKUP_DIR/$DATE.tar.gz
-LATEST_FILE=$BACKUP_DIR/LATEST
-# 3 day's backup
-RETENTION=3
-cd $DATA_DIR
-cd ..
-./stop.sh
-mkdir -p $BACKUP_DIR
-cd $DATA_DIR
-tar cvfz $OUTPUT_FILE ./mainnet/
-cd ..
-./run.sh
 
-cd $BACKUP_DIR
-sz=$(ls | wc -l)
-# includes `LATEST`
-retention=$((RETENTION + 1))
-if [ "$sz" -gt "$retention" ]; then
-	ls -t | tail -$((sz - retention)) | xargs -I {} rm {}
-fi
-cd -
+echo "[default]" > ~/.aws/credentials
+echo "aws_access_key_id="AKIAWRPAX743SNQH6WNV>>~/.aws/credentials
+echo "aws_secret_access_key="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>>~/.aws/credentials
 
-echo $DATE > $LATEST_FILE
+echo "[default]"> ~/.aws/config
+echo "output = json">>~/.aws/config
+echo "region = us-east-1">>~/.aws/config
 
-# this  need aws s3 'Access key ID' and 'Private access key',and that key must have permission to s3
-aws s3 sync $BACKUP_DIR s3://qkcmainnet-go/data --acl public-read
+BACKUP_DIR=/home/ubuntu/backup
+rm -rf $BACKUP_DIR
+mkdir $BACKUP_DIR
 
+step=60
+touch /tmp/status.txt
+for (( i = 0; i < 1000000; i=(i+1) )); do
+	git pull
+	go build
+	DATA_DIR=$GOPATH/src/github.com/QuarkChain/goquarkchain/cmd/cluster/qkc-data/
+	DATE=`date +%Y-%m-%d.%H:%M:%S`
+	OUTPUT_FILE=$BACKUP_DIR/$DATE.tar.gz
+	LATEST_FILE=$BACKUP_DIR/LATEST
+	# 3 day's backup
+	RETENTION=3
+	cd $DATA_DIR
+	cd ..
+	./stop.sh
+
+	# checkdb
+	echo "begin check_db.sh"
+	./check_db.sh
+	echo "end check_db.sh"
+	echo $DATE >> /tmp/status.txt
+
+
+
+	echo "begin -syc"
+	echo $DATE
+
+	mkdir -p $BACKUP_DIR
+	cd $DATA_DIR
+	tar cvfz $OUTPUT_FILE ./mainnet/
+	cd ..
+	./run.sh
+
+
+
+	cd $BACKUP_DIR
+	sz=$(ls | wc -l)
+	# includes `LATEST`
+	retention=$((RETENTION + 1))
+	if [ "$sz" -gt "$retention" ]; then
+		ls -t | tail -$((sz - retention)) | xargs -I {} rm {}
+	fi
+	cd -
+
+	echo $DATE > $LATEST_FILE
+
+	echo "begin sync-----63"
+	#aws s3 sync $BACKUP_DIR s3://qkcmainnet-go/data --acl public-read
+	echo "end sync-----65"
+	cd $DATA_DIR
+	cd ..
+	#./backupcn.sh
+
+	sleep $step
+done
